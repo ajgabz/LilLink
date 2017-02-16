fe25ddff78ecaf2b7aa458b118cbde7b9cdfed60
Aaron Gaba <airplane679@gmail.com>
2017-02-16 15:04:56 -0500

    Test Code for Handling Routing and Redis

--- lil_link/lil_link.py
+++ lil_link/lil_link.py
@@ -1,9 +1,32 @@
-from flask import Flask
+from flask import Flask, redirect, request, render_template
+import redis
+
 app = Flask(__name__)
+r = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 @app.route('/')
 def hello_world():
    return 'Hello, World!'
+
+@app.route('/redirect-test')
+def redirectionTest():
+    return redirect("http://cs.mcgill.ca/~agaba/")
+
+@app.route('/cow')
+def cow():
+    return 'Moo!'
+
+@app.route('/centeredText/<userText>')
+def templateTest(userText):
+    return render_template('test.html', centeredText = userText)
+
+@app.route('/<key>')
+def keyTest(key):
+    value = r.get(key)
+    if (value is None):
+        return "GET('" + key + "') --> None"
+    else:
+        return "GET('" + key + "') --> " + value
 
 if __name__ == '__main__':
    app.run()
