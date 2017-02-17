ccebed1766563f6ecf92cd3c81238c0d4ef7a6dd
Aaron Gaba <airplane679@gmail.com>
2017-02-17 12:14:38 -0500

    Removed Skeleton, Imported Bootstrap, and Wrote Base Code for System

--- lil_link/lil_link.py
+++ lil_link/lil_link.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 r = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 @app.route('/')
-def hello_world():
-   return 'Hello, World!'
+def index():
+   return render_template('index.html')
 
 @app.route('/redirect-test')
 def redirectionTest():
